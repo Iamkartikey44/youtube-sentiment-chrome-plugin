7742be89b67de354434563e3526a71582b7c41cd
MLPlayground-KT <kartikey.tiwari.cs.2018@miet.ac.in>
2026-01-20 23:24:17 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A lightweight Chrome extension that extracts YouTube comments in real time and applies NLP-based sentiment analysis to display sentiment insights directly on the YouTube interface.',
+    author='Kartikey',
+    license='',
+)
